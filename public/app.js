850c1fdba2843096e569cb1f407eb16678db2b88
Ishan Thilina Somasiri <ishanthilina@gmail.com>
2014-07-13 21:16:43 +0530

    shows the returned data in a raw way

--- public/app.js
+++ public/app.js
@@ -3,11 +3,11 @@ var app = angular.module('NICApp', [ ]);
 app.controller("MainController", [ '$http', '$scope', function($http,$scope){
     $scope.nicNumInput = "";
 
+
     $scope.getInfo = function() {
 		$http.get('/info?id='+$scope.nicNumInput)
 			.success(function(data) {
-				// $scope.formData = {}; // clear the form so our user is ready to enter another
-				// $scope.todos = data;
+				$scope.nicData=data;
 				console.log(data);
 			})
 			.error(function(data) {
