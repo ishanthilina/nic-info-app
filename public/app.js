91683eacf24685cd43708291e181a02ec7459d37
Ishan Thilina Somasiri <ishanthilina@gmail.com>
2014-07-13 13:39:54 +0530

    can get the request from the api using angular

--- public/app.js
+++ public/app.js
@@ -1,5 +1,18 @@
 var app = angular.module('NICApp', [ ]);
 
-app.controller("MainController", function($scope){
+app.controller("MainController", [ '$http', '$scope', function($http,$scope){
     $scope.nicNumInput = "";
-});
+
+    $scope.getInfo = function() {
+		$http.get('/info?id='+$scope.nicNumInput)
+			.success(function(data) {
+				// $scope.formData = {}; // clear the form so our user is ready to enter another
+				// $scope.todos = data;
+				console.log(data);
+			})
+			.error(function(data) {
+				console.log('Error: ' + data);
+			});
+	};
+
+}]);
